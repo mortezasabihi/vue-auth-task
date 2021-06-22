1c8b864b6c35de1e4318e314d39f2076ae964ff5
Morteza Sabihi <mortezasabihi87@gmail.com>
2021-06-22 11:07:43 +0430

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- navbar -->
+  <Navbar />
+  <!-- /navbar -->
+</template>
+
+<script>
+import { Navbar } from "@/components/Layout";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
